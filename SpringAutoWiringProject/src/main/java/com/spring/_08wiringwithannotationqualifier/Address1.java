0e886c94f28cf799ffb3afdfc31f1fd0c4479246
Taher <Taher@192.168.1.6>
2019-01-03 16:35:36 +0530

    Spring

---
+++ SpringAutoWiringProject/src/main/java/com/spring/_08wiringwithannotationqualifier/Address1.java
@@ -0,0 +1,17 @@
+package com.spring._08wiringwithannotationqualifier;
+
+public class Address1 implements Address {
+
+	private String city;
+	private String state;
+
+	public Address1(String city, String state) {
+		super();
+		this.city = city;
+		this.state = state;
+	}
+
+	public String empAddress() {
+		return city + " " + state;
+	}
+}
---
+++ SpringAutoWiringProject/src/main/java/com/spring/_08wiringwithannotationqualifier/Address2.java
@@ -0,0 +1,17 @@
+package com.spring._08wiringwithannotationqualifier;
+
+public class Address2 implements Address {
+
+	private String city;
+	private String state;
+
+	public Address2(String city, String state) {
+		super();
+		this.city = city;
+		this.state = state;
+	}
+
+	public String empAddress() {
+		return city + " " + state;
+	}
+}
